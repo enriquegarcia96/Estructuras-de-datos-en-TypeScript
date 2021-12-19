9d4297fb4848fdc5be6c41d31b22c2a5cdd76225
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-12-19 12:46:34 -0600

    feat: 🎸 ejecicios de revicion de paquetes de encimiendas

---
+++ Envios-de-paquetes/index.ts
@@ -0,0 +1,42 @@
+const reservarUnidades: number = 30;
+let TamanoCamion: number = 0;
+const ArrayPaquetes: number[] = [];
+const nuevoArray: number[] =[];
+
+let paquete1: number = 0;
+let paquete2: number = 0; 
+
+let contador: number  = 1;
+TamanoCamion = parseInt( window.prompt('Ingrese el tamaño del camion'));
+
+do {
+
+    let paquete  = parseInt( window.prompt(`ingrese el paquete ${contador}  a cargar`));
+    ArrayPaquetes.push(paquete);
+    contador++;    
+    
+} while (contador <= 5);
+
+
+/**
+ * Funciones para calcular el espacio
+ */
+const calcularEspacio = (tamanoCamion: number, reservarUnidades: number) => tamanoCamion - reservarUnidades
+const calculaSumaNuevoArray = (nuevoArray: number[] ) => nuevoArray.reduce( (a, b) => a + b, 0);  
+
+for (let i = 0; i < ArrayPaquetes.length-1; i++) {
+    for (let j = i+1; j < ArrayPaquetes.length; j++) {
+        if(ArrayPaquetes[i] != ArrayPaquetes[j]){
+            if(( ArrayPaquetes[i] + ArrayPaquetes[j] ) == calcularEspacio(TamanoCamion, reservarUnidades)){
+                paquete1 = ArrayPaquetes[i];
+                paquete2 = ArrayPaquetes[j];
+                nuevoArray.push(paquete1, paquete2);
+                console.log('Tamaño del Camion => ', TamanoCamion);
+                console.log('paquetes ingresado => ', ArrayPaquetes);
+                console.log(`Resultado [${nuevoArray}] La suma de los paquetes es ${calculaSumaNuevoArray(nuevoArray)},\nlo que permite dejar las ${reservarUnidades} unidades de espacio requeridos ` + nuevoArray)
+            }
+        }else{
+            console.log('No se puede escojer el mismo paquete dos veces')
+        }
+    }
+}
